1887dc22ebd73db10952ed5dd18dd2bcf10f7612
Anthony Powles <github@idreamz.net>
2017-04-19 18:20:39 -0700

    Add example to use VPC module

---
+++ examples/vpc/variables.tf
@@ -0,0 +1,9 @@
+
+variable "access_key" { 
+  description = "AWS access key"
+}
+
+variable "secret_key" {
+  description = "AWS secret access key"
+}
+
---
+++ examples/vpc/vpc.tf
@@ -0,0 +1,8 @@
+
+module "vpc" {
+  source = "../../vpc"
+
+  access_key  = "${var.access_key}"
+  secret_key  = "${var.secret_key}"
+}
+
