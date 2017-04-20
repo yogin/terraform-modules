7ad8343e2435c08f83c8fab920c2c49285b5117d
Anthony Powles <github@idreamz.net>
2017-04-19 19:50:05 -0700

    Update vpc_bastion example, separate AMIS module into it's own file

---
+++ examples/vpc_bastion/amis.tf
@@ -0,0 +1,9 @@
+
+module "amis" {
+  source = "../../amis"
+
+  access_key                  = "${var.access_key}"
+  secret_key                  = "${var.secret_key}"
+  region                      = "${var.region}"
+}
+
--- examples/vpc_bastion/bastion.tf
+++ examples/vpc_bastion/bastion.tf
@@ -1,12 +1,4 @@
 
-module "amis" {
-  source = "../../amis"
-
-  access_key                  = "${var.access_key}"
-  secret_key                  = "${var.secret_key}"
-  region                      = "${var.region}"
-}
-
 module "bastion" {
   source = "../../bastion"
 
